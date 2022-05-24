27cecdadcb30aeb6ac854e1deaadb51446ef3aea
AJ <pv78dpg@eduvaud.ch>
2022-05-24 13:11:55 +0200

    Fixed users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 72);
             $table->rememberToken();
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
