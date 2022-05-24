bdd752403885bbf2be5cc333e51e82cac253e4ee
AJ <pv78dpg@eduvaud.ch>
2022-05-24 08:30:16 +0200

    Updated UserSeeder
    Added default admin user for testing purposes.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,5 +19,14 @@ class UserSeeder extends Seeder
         User::factory()
                 ->count(20)
                 ->create();
+
+        User::create([
+            'firstname' => 'Ahmad',
+            'lastname' => 'Jano',
+            'gender' => 'male',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'is_admin' => 1,
+        ]);
     }
 }
