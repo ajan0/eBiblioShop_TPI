16381b923e6b869d9412baa1f7b493716fba8c84
AJ <pv78dpg@eduvaud.ch>
2022-05-24 08:16:38 +0200

    Updated Book model
    - Informed laravel to cast field `published_at` as a Carbon date object.
    - Established relationships to the following models: Category, Author, Editor, User.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $dates = [
+        'publish_at'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+
+    public function editor()
+    {
+        return $this->belongsTo(Editor::class);
+    }
+
+    public function uploaded_by()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
